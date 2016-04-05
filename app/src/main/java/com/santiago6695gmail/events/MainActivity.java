56ad55f72d3a6730260141fffe16530d171d85cb
Santiago Torres <santiago6695@gmail.com>
2016-04-05 18:00:36 -0400

    added functionality to sign up button

--- app/src/main/java/com/santiago6695gmail/events/MainActivity.java
+++ app/src/main/java/com/santiago6695gmail/events/MainActivity.java
@@ -2,9 +2,12 @@ package com.santiago6695gmail.events;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,12 +15,19 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.category_list);
+        setContentView(R.layout.sign_in);
         //hide title and icon in action bar
         ActionBar actionBar = getActionBar();
         actionBar.show();
-      // actionBar.setDisplayShowTitleEnabled(false);
-      // actionBar.setDisplayUseLogoEnabled(false);
+        // actionBar.setDisplayShowTitleEnabled(false);
+        // actionBar.setDisplayUseLogoEnabled(false);
+        //set up buttons
+        Button signUpButton = (Button) findViewById(R.id.signup);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.sign_up);
+            }
+        });
     }
 
     @Override
@@ -26,12 +36,5 @@ public class MainActivity extends Activity {
         return true;
     }
     //set up the menu
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
 
